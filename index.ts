5b799ec7e8f25cde371db5dd9738d6d7a837ad81
jackthomsonn <hello@jackthomson.co.uk>
2020-12-31 13:12:58 +0000

    Export AccountType

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
+import { AccountType } from './enums/account_type/index';
 import { IJob } from './interfaces/IJob';
 import { IUser } from './interfaces/IUser';
 import { JobStatus } from './enums/job_status';
-import { Role } from './enums/role';
 import { VerificationStatus } from './enums/verification_status';
 
-export { IJob, IUser, Role, JobStatus, VerificationStatus };
+export { IJob, IUser, JobStatus, VerificationStatus, AccountType };
