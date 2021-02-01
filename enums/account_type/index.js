0d1b2a49d1eedab46e0b7b6519db5cbb95dd5fb9
jackthomsonn <hello@jackthomson.co.uk>
2021-02-01 20:04:24 +0000

    Stable 2

---
+++ enums/account_type/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.AccountType = void 0;
+var AccountType;
+(function (AccountType) {
+    AccountType["BUSINESS"] = "business";
+    AccountType["RIDER"] = "rider";
+    AccountType["PLATFORM"] = "platform";
+    AccountType["NONE"] = "none";
+})(AccountType = exports.AccountType || (exports.AccountType = {}));
---
+++ enums/verification_status/index.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.VerificationStatus = void 0;
+var VerificationStatus;
+(function (VerificationStatus) {
+    VerificationStatus["COMPLETED"] = "complete";
+    VerificationStatus["FAILED"] = "failed";
+    VerificationStatus["CANCELLED"] = "cancelled";
+    VerificationStatus["PENDING"] = "pending";
+    VerificationStatus["NEEDS_VERIFICATION"] = "awaiting_verification";
+})(VerificationStatus = exports.VerificationStatus || (exports.VerificationStatus = {}));
