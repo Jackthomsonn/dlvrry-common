7cf44ea0d7452711c6984a1830edc98248a76cb2
jackthomsonn <hello@jackthomson.co.uk>
2021-03-13 11:58:13 +0000

    Stable

--- enums/mode_type/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ModeType = void 0;
-var ModeType;
-(function (ModeType) {
-    ModeType["CAR"] = "car";
-    ModeType["BIKE"] = "bike";
-    ModeType["MOPED"] = "moped";
-})(ModeType = exports.ModeType || (exports.ModeType = {}));
